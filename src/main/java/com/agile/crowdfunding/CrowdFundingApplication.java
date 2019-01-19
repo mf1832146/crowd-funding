649adbe8341b69cd9240c6cbf0569f042782864f
mf1832146 <tangze87811769>
2019-01-19 15:43:55 +0800

    [ADD] 新建项目

---
+++ src/main/java/com/agile/crowdfunding/CrowdFundingApplication.java
@@ -0,0 +1,14 @@
+package com.agile.crowdfunding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdFundingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrowdFundingApplication.class, args);
+    }
+
+}
+
