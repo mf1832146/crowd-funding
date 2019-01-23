d81c4cd12c417703aa41b45d9e81b0a131afcf46
mf1832146 <tangze87811769>
2019-01-23 20:25:25 +0800

    [MOD] 修改Bug

--- src/main/java/com/agile/crowdfunding/dao/ProjectRepository.java
+++ src/main/java/com/agile/crowdfunding/dao/ProjectRepository.java
@@ -11,12 +11,7 @@ import java.util.List;
  */
 @Repository
 public interface ProjectRepository extends JpaRepository<Project,String> {
-
-<<<<<<< HEAD
-
-=======
     public List<Project> findAll();
 
     public Project findByProjectId(String projID);
->>>>>>> master
 }
